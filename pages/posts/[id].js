3a1f75440fb3384b6ba46c73939ce6af8c797175
ria <riatyagi28@gmail.com>
2021-07-22 13:36:37 +0530

    feat:dynamic path

---
+++ pages/posts/[id].js
@@ -0,0 +1,9 @@
+import Posts from '../../lib/posts'
+
+export default function Post() {
+  return <Posts>...</Posts>
+}
+
+export async function getStaticPaths() {
+    // Return a list of possible value for id
+  }
